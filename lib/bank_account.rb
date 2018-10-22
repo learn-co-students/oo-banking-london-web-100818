e12cd8527295b235b50dd5a4e1b41d2fd3176327
Flatiron School <damanybailey@icloud.com>
2018-10-22 18:38:05 +0100

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,30 @@
 class BankAccount
 
+    attr_accessor :balance, :status
+    attr_reader :name
+
+    def initialize(name)
+        @balance = 1000
+        @status = 'open'
+        @name = name
+    end
+
+    def deposit(amount)
+        @balance += amount
+    end
+
+    def display_balance
+        "Your balance is $#{self.balance}."
+    end
+
+    def valid?
+        if self.status == "open" && self.balance > 0
+            return true
+        end
+            false
+    end
+
+    def close_account
+        self.status = "closed"
+    end
 end
